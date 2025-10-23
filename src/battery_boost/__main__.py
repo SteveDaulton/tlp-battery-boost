fef8bfef3a0b1bb937ca05261b2c510776a7b021
SteveDaulton <SteveDaulton@users.noreply.github.com>
2025-10-23 16:40:36 +0100

    Refactor constants into separate module

--- src/battery_boost/__main__.py
+++ src/battery_boost/__main__.py
@@ -14,93 +14,18 @@ import sys
 import tkinter as tk
 
 from collections import defaultdict
-from enum import Enum
 from tkinter import ttk
 from tkinter import simpledialog, messagebox
-from typing import Callable, TypedDict, NoReturn, TypeAlias
+from typing import Callable, NoReturn, TypeAlias
 
-
-# 10 min interval between re-validating sudo password
-REFRESH_INTERVAL_MS = 600_000
-
-
-class ThemeName(Enum):
-    """Available themes."""
-    LIGHT = 'light'
-    DARK = 'dark'
-
-
-ThemeKeys = TypedDict('ThemeKeys', {'background': str,
-                                    'active': str,
-                                    'text': str,
-                                    'button_0': str,
-                                    'active_0': str,
-                                    'button_1': str,
-                                    'active_1': str})
-
-THEME: dict[ThemeName, ThemeKeys] = {
-    ThemeName.LIGHT: {'background': 'white',
-                      'active': 'gold',
-                      'text': 'black',
-                      'button_0': 'gainsboro',
-                      'active_0': 'darkgray',
-                      'button_1': 'goldenrod',
-                      'active_1': 'darkgoldenrod'},
-    ThemeName.DARK: {'background': 'gray15',
-                     'active': 'firebrick4',
-                     'text': 'white',
-                     'button_0': 'gray30',
-                     'active_0': 'gray40',
-                     'button_1': 'firebrick',
-                     'active_1': 'firebrick3'}
-    }
-
-
-DEFAULT_THEME = THEME[ThemeName.LIGHT]
-
-
-FONT_SIZES = {1: (('TkDefaultFont', 8), ('TkDefaultFont', 7), 0.72),
-              2: (('TkDefaultFont', 10), ('TkDefaultFont', 8), 0.84),
-              3: (('TkDefaultFont', 12), ('TkDefaultFont', 10), 1.0),
-              4: (('TkDefaultFont', 14), ('TkDefaultFont', 12), 1.2),
-              5: (('TkDefaultFont', 18), ('TkDefaultFont', 14), 1.4)}
-
-
-class BatteryState(Enum):
-    """TLP states."""
-    DEFAULT = 'default'
-    RECHARGE = 'recharge'
-
-
-UIState = TypedDict('UIState', {'action': str,
-                                'label_text': str,
-                                'button_text': str})
-
-
-STATES: dict[BatteryState, UIState] = {
-    BatteryState.DEFAULT: {
-        'action': "TLP reset to current defaults.\n\n",
-        'label_text': "Default TLP profile",
-        'button_text': "Click to Recharge"
-        },
-    BatteryState.RECHARGE: {
-        'action': "Charging to full capacity.\n\n",
-        'label_text': "Full Recharge Enabled",
-        'button_text': "Switch to Default"
-        }
-    }
-
-
-STATE_COLOR_KEYS = {
-    BatteryState.DEFAULT: {
-        'bg': 'button_0',
-        'active': 'active_0',
-    },
-    BatteryState.RECHARGE: {
-        'bg': 'button_1',
-        'active': 'active_1',
-    },
-}
+from battery_boost.constants import (REFRESH_INTERVAL_MS,
+                                     THEME,
+                                     ThemeName,
+                                     ThemeKeys,
+                                     DEFAULT_THEME,
+                                     FONT_SIZES,
+                                     BatteryState,
+                                     STATES)
 
 
 class App(tk.Tk):  # pylint: disable=too-many-instance-attributes
